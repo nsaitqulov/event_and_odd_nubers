eb2cafeb951bf32774b48f93e1a8d2ec6a3066bc
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-04 21:26:17 +0500

    sum_odd.py

--- sum_odd.py
+++ sum_odd.py
@@ -1,22 +1,22 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
-var_int=4563
-#Create a variable "var_int" and assign it a four-digit integer value.
+var_int=9222
 sum_even=0
+#Create a variable "var_int" and assign it a four-digit integer value.
+a=var_int%10
+sum_even+=a%2*a
+var_int//=10
+
+b=var_int%10
+sum_even+=b%2*b
+var_int//=10
+
+c=var_int%10
+sum_even+=c%2*c
+var_int//=10
+
+d=var_int%10
+sum_even+=d%2*d
+var_int//=10
 #Create a variable "sum_even" and assign it 0.
-a1=var_int%10
-sum_even+=a1%2*a1
-var_int//=10
-
-a2=var_int%10
-sum_even+=a2%2*a2
-var_int//=10
-
-a3=var_int%10
-sum_even+=a3%2*a3
-var_int//=10
-
-a4=var_int%10
-sum_even+=a4%2*a4
-var_int//=10
 #Find the sum of the odd digits in the variable "var_int".
 print(sum_even)
